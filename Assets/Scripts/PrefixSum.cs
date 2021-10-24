de14916f4d833a26c9404446602e8a14d845ef88
George Psomathianos <45467851+GeorgePsom@users.noreply.github.com>
2021-10-24 22:56:49 +0100

    NumOfPolygons and Prefix Shader

--- Assets/Scripts/PrefixSum.cs
+++ Assets/Scripts/PrefixSum.cs
@@ -16,19 +16,30 @@ public class PrefixSum : MonoBehaviour{
         for (int i = 0; i < arraySize; i++){
             pSum[i] = i;
         }
-        
+
+        int iter = (int)System.Math.Ceiling(System.Math.Log(arraySize, 2))-1;
         ComputeBuffer pSumBuffer = new ComputeBuffer(arraySize, sizeof(int));
-        pSumBuffer.SetData(pSum);
+        ComputeBuffer initPSumBuffer = new ComputeBuffer(arraySize, sizeof(int));
+        
         prefixSumCs.SetInt("arraySize", arraySize);
+        prefixSumCs.SetBuffer(0, "initPSum", initPSumBuffer);
+        initPSumBuffer.SetData(pSum);
         
-        prefixSumCs.SetBuffer(0, "pSum", pSumBuffer);
-        prefixSumCs.Dispatch(0, 1, 1, 1);
-        
-        pSumBuffer.GetData(pSum);
-        
-        for (int i = 0; i < arraySize; i++){
-            Debug.Log(i + " - " + pSum[i]);
+        for (int i = 0; i < iter+1; i++){
+            pSumBuffer.SetData(pSum);
+            prefixSumCs.SetBuffer(0, "pSum", pSumBuffer);
+            prefixSumCs.SetInt("iterNum", i);
+            
+            prefixSumCs.Dispatch(0, 2048/8, 1, 1);
+            
+            pSumBuffer.GetData(pSum);
+            int x = 5;
         }
+
+        /*for (int i = 0; i < arraySize; i++){
+            Debug.Log(i + " - " + pSum[i]);
+        }*/
+        Debug.Log("Done");
     }
 
     // Update is called once per frame
